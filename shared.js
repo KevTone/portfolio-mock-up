c57cd11016ea32f08e425d089dd070a2cf5c3aca
KevTone <80661509+KevTone@users.noreply.github.com>
2021-04-07 23:41:22 -0400

    navbar complete except toggle opening mobile-nav. Positioning and font styles added to welcome section.

--- shared.js
+++ shared.js
@@ -1,6 +1,9 @@
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
 
-toggleButton.addEventListener("click", function() {
+console.dir('.mobile-nav');
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
     mobileNav.classList.add("open");
 });
